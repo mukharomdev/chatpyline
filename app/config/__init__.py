a6aecfef24fa5d8eb575ef586536b4cdb122792b
mukharomdev <mukharomdev@gmail.com>
2025-04-04 09:18:00 +0700

    update app/config dan env-example

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -10,7 +10,7 @@ lineChannelAccessToken=os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 
 nlpClientToken=os.getenv('WITAI_CLIENT_TOKEN', None)
 nlpClientUrl=os.getenv('WITAI_API_URL', None)
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://user:password@localhost:5432/linebot_db')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', None)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # __ALL__ = [lineChannelSecret,lineChannelAccessToken,nlpClientToken,nlpClientUrl]
