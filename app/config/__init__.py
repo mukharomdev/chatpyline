69537e614fcdfc0cdbc7b3eb2d61f7601f26735b
mukharomdev <mukharomdev@gmail.com>
2025-04-05 23:59:37 +0700

    update app

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -16,5 +16,17 @@ class Config:
 
 	SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', None)
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # PostgreSQL di produksi
+# Pilih config berdasarkan environment
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
 
 __all__ = [Config]
